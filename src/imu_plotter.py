cc0d9ab3c79ecbcd60794a15bc3d9d2d569f6974
Soonhwan Kwon <shk01421@naver.com>
2025-05-30 22:55:13 +0900

    작은수정

--- src/imu_plotter.py
+++ src/imu_plotter.py
@@ -17,33 +17,41 @@ imu = pd.read_csv(
 imu = imu.sort_values(by="timestamp")
 
 # --- 3) 3행짜리 서브플롯 생성 ---
-fig, axes = plt.subplots(3, 1, figsize=(12, 12), sharex=True)
-fig.suptitle("IMU: Acceleration, Angular Velocity, and Angles")
+fig, axes = plt.subplots(5, 1, figsize=(12, 14), sharex=True)
+fig.suptitle("IMU: Acceleration, Angular Velocity")
 
 # 가속도
 axes[0].plot(imu["timestamp"], imu["accX"], label="accX")
-axes[0].plot(imu["timestamp"], imu["accY"], label="accY")
-axes[0].plot(imu["timestamp"], imu["accZ"], label="accZ")
 axes[0].set_ylabel("Acceleration [m/s²]")
 axes[0].legend(loc="upper right")
 axes[0].grid(True)
 
-# 각속도
-axes[1].plot(imu["timestamp"], imu["gyroX"], label="gyroX")
-axes[1].plot(imu["timestamp"], imu["gyroY"], label="gyroY")
-axes[1].plot(imu["timestamp"], imu["gyroZ"], label="gyroZ")
-axes[1].set_ylabel("Angular Velocity [°/s]")
+axes[1].plot(imu["timestamp"], imu["accY"], label="accY")
+axes[1].set_ylabel("Acceleration [m/s²]")
 axes[1].legend(loc="upper right")
 axes[1].grid(True)
 
-# 각도
-axes[2].plot(imu["timestamp"], imu["angleX"], label="angleX")
-axes[2].plot(imu["timestamp"], imu["angleY"], label="angleY")
-axes[2].plot(imu["timestamp"], imu["angleZ"], label="angleZ")
-axes[2].set_ylabel("Angle [°]")  
-axes[2].set_xlabel("Timestamp [s]")
+axes[2].plot(imu["timestamp"], imu["accZ"], label="accZ")
+axes[2].set_ylabel("Acceleration [m/s²]")
 axes[2].legend(loc="upper right")
 axes[2].grid(True)
+
+# 각속도
+axes[3].plot(imu["timestamp"], imu["gyroX"], label="gyroX")
+axes[3].plot(imu["timestamp"], imu["gyroY"], label="gyroY")
+axes[3].plot(imu["timestamp"], imu["gyroZ"], label="gyroZ")
+axes[3].set_ylabel("Angular Velocity [°/s]")
+axes[3].legend(loc="upper right")
+axes[3].grid(True)
+
+# 각도
+# axes[2].plot(imu["timestamp"], imu["angleX"], label="angleX")
+# axes[2].plot(imu["timestamp"], imu["angleY"], label="angleY")
+# axes[2].plot(imu["timestamp"], imu["angleZ"], label="angleZ")
+# axes[2].set_ylabel("Angle [°]")  
+# axes[2].set_xlabel("Timestamp [s]")
+# axes[2].legend(loc="upper right")
+# axes[2].grid(True)
 
 plt.tight_layout(rect=[0, 0.03, 1, 0.95])
 plt.show()
