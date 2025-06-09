2eacf82b299dc51a415d77fb91451bb816bf1258
wha-hat <tleks123@naver.com>
2025-06-09 16:28:55 +0900

    jsut maneuver 시간차로 수정

--- src/just_maneuver.py
+++ src/just_maneuver.py
@@ -6,6 +6,7 @@ from sl_valve import open_l, open_r, close_l, close_r
 from watcher import func_watcher
 from yaw_estimator import yaw_estimator
 from linear_act import foward,stop,stopstop,right_turn,left_turn, backward
+import threading
 
 #######
 #####
@@ -16,35 +17,36 @@ from linear_act import foward,stop,stopstop,right_turn,left_turn, backward
 
 # 아두이노연결 (open_serial 함수 이용 간단하게)
 ser = open_serial(port="COM8", baudrate=115200)  #아두이노우노, IMU센서
-ser1= open_serial(port="COM6", baudrate=115200)  #아두이노메가-전방
-ser2= open_serial(port="COM7", baudrate=115200)  #아두이노메가-후방 mosfet모듈(솔레노이드밸브) 2개, 모터드라이버(리니어 액츄에이터) 1개 
+ser_steer= open_serial(port="COM6", baudrate=115200)  #아두이노메가-전방
+ser_rear= open_serial(port="COM7", baudrate=115200)  #아두이노메가-후방 mosfet모듈(솔레노이드밸브) 2개, 모터드라이버(리니어 액츄에이터) 1개 
 
 #초기세팅(각 솔레노이드밸브 전부 닫아두기 + 리니어액츄에이터로 브레이크 미리 밟아두기 추가 예정)
 imu_log = open_l("imu_log.csv", "w")
 imu_log_list=[]
 delta_vlist=[] 
 
-close_l(ser2)   #좌측후방
-close_r(ser2)   #우측후방
-foward(ser2)
+close_l(ser_rear)   #좌측후방
+close_r(ser_rear)   #우측후방
+foward(ser_rear)
 
 print('7초 뒤에 미셈셈')
 time.sleep(7)
 print('미셈')
 time.sleep(1.5)
 
-open_l(ser2)
-backward(ser1)
+open_l(ser_rear)
+left_turn(ser_steer)
 time.sleep(1)
-stop(ser1)          
+stop(ser_steer)          
 
 
 
-backward(ser2)
-time.sleep(0.1)
-stop(ser2)
-close_l(ser2)
-open_r(ser2)
-foward(ser1)  
-time.sleep(1)
-stop(ser1)
+right_turn(ser_steer)  
+threading.Timer(1.2, lambda: stopstop(ser_steer)).start()
+open_r(ser_rear)
+threading.Timer(0.1, lambda: close_r(ser_steer)).start()
+backward(ser_rear)
+threading.Timer(0.1, lambda: foward(ser_rear)).start()
+threading.Timer(0.1, lambda: close_l(ser_steer)).start()
+threading.Timer(1.5,lambda:open_r(ser_rear)).start()
+yaw_handled= True
